57609c76d95c90698c7642769e62241c45c13022
thorlrsn <thorlarsen96@gmail.com>
2022-11-15 16:37:40 +0100

    new file:   Models/adam_20epoch_163264/model.h5 	new file:   Models/adam_20epoch_163264/model.json 	new file:   Models/adam_20epoch_163264/results.png

--- goal_detector.py
+++ goal_detector.py
@@ -4,9 +4,15 @@ import PIL
 import tensorflow as tf
 import pathlib
 import requests
+import cv2
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
+
+### FLAGS ###
+show_sample_data_flag = False
+show_results_flag = False
+model_save_flag = False
 
 batch_size = 32
 img_height = 180
@@ -52,13 +58,15 @@ class_names = ['Football goal', 'Not football goal']
 # class_names = train_ds.class_names
 
 ### Visualise some of the training data
-# plt.figure(figsize=(10, 10))
-# for images in train_ds.take(1):
-#   for i in range(9):
-#     ax = plt.subplot(3, 3, i + 1)
-#     plt.imshow(images[i].numpy().astype("uint8"))
-#     plt.axis("off")
-# plt.show()
+if show_sample_data_flag is True:
+    plt.figure(figsize=(10, 10))
+    for images, labels in train_ds.take(1):
+        for i in range(9):
+            ax = plt.subplot(3, 3, i + 1)
+            plt.imshow(images[i].numpy().astype("uint8"))
+            plt.title(class_names[labels[i]])
+            plt.axis("off")
+        plt.show()
 
 ### Setting cache and prefetch, to optimise performance
 AUTOTUNE = tf.data.AUTOTUNE
@@ -81,7 +89,7 @@ model = Sequential([
   layers.Flatten(),
   layers.Dense(128, activation='relu'),
   layers.Dense(num_classes)
-
+  
 ])
 
 model.compile(optimizer='adam',
@@ -90,7 +98,7 @@ model.compile(optimizer='adam',
 
 model.summary()
 
-epochs=20
+epochs=10
 history = model.fit(
   train_ds,
   validation_data=val_ds,
@@ -103,19 +111,46 @@ val_acc = history.history['val_accuracy']
 loss = history.history['loss']
 val_loss = history.history['val_loss']
 
-epochs_range = range(epochs)
+### Test model on sample image
+img=cv2.imread(r"C:\Users\thorl\OneDrive - Danmarks Tekniske Universitet\thor\3. Semester\Deep learning\project\DP-project\Data\Football goal\download (9).jpeg")
+image=cv2.resize(img, (180,180))
+image=np.expand_dims(image, axis=0) #input shape needs to be (1,width,height,channels)
+predictions = model.predict(image)
+print(predictions)
+class_index = np.argmax(predictions)
+print(class_index)
 
-plt.figure(figsize=(8, 8))
-plt.subplot(1, 2, 1)
-plt.plot(epochs_range, acc, label='Training Accuracy')
-plt.plot(epochs_range, val_acc, label='Validation Accuracy')
-plt.legend(loc='lower right')
-plt.title('Training and Validation Accuracy')
+# sess = keras.backend.get_session()
+# img = tf.image.decode_jpeg(img, channels=3)
+# img.set_shape([None, None, 3])
+# img = tf.image.resize_images(img, (32, 32))
+# img = img.eval(session=sess) # convert to numpy array
+# img = np.expand_dims(img, 0) # make 'batch' of 1
+# pred = model.predict(img)
+# pred = class_names["label_names"][np.argmax(pred)]
 
-plt.subplot(1, 2, 2)
-plt.plot(epochs_range, loss, label='Training Loss')
-plt.plot(epochs_range, val_loss, label='Validation Loss')
-plt.legend(loc='upper right')
-plt.title('Training and Validation Loss')
-plt.show()
+if show_results_flag is True:
+    epochs_range = range(epochs)
+    plt.figure(figsize=(8, 8))
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs_range, acc, label='Training Accuracy')
+    plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+    plt.legend(loc='lower right')
+    plt.title('Training and Validation Accuracy')
 
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs_range, loss, label='Training Loss')
+    plt.plot(epochs_range, val_loss, label='Validation Loss')
+    plt.legend(loc='upper right')
+    plt.title('Training and Validation Loss')
+    plt.show()
+
+### Saving model
+if model_save_flag is True:
+    # serialize model to JSON
+    model_json = model.to_json()
+    with open("model.json", "w") as json_file:
+        json_file.write(model_json)
+    # serialize weights to HDF5
+    model.save_weights("model.h5")
+    print("Saved model to disk")
---
+++ goal_test.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import PIL
+import tensorflow as tf
+# import pathlib
+# import requests
+import cv2
+# from tensorflow import keras
+# from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential, model_from_json
+# from tensorflow.keras.layers import Dense
+
+### Load model from folders
+path_to_dir = r"C:\Users\thorl\OneDrive - Danmarks Tekniske Universitet\thor\3. Semester\Deep learning\project\DP-project\Models\adam_20epoch_163264"
+path_to_model = path_to_dir + '\model.json'
+path_to_weight = path_to_dir + '\model.h5'
+
+json_file = open(path_to_model, 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+loaded_model.load_weights(path_to_weight)
+print("Loaded model from disk")
+
+### Compile the model
+loaded_model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+### Test on single image
+image=cv2.imread(r"C:\Users\thorl\OneDrive - Danmarks Tekniske Universitet\thor\3. Semester\Deep learning\project\DP-project\Data\Football goal\images (90).jpeg")
+image=cv2.resize(image, (180,180))
+image=np.expand_dims(image, axis=0) #input shape needs to be (1,width,height,channels)
+predictions = loaded_model.predict(image)
+print(predictions)
+
+
